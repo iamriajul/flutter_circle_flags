6566ae18c31c11a38260076a2be9cfda41e84e68
iamriajul <kmriajulislami@gmail.com>
2022-12-05 18:49:20 +0600

    feat: simplified.

--- lib/circle_flags.dart
+++ lib/circle_flags.dart
@@ -12,14 +12,17 @@ class CircleFlag extends StatelessWidget {
 
   final Color? placeholderBackgroundColor;
   final Color? placeholderTextColor;
+  final String assetName;
 
-  const CircleFlag(this.countryCode,
+  CircleFlag(this.countryCode,
       {Key? key,
       this.size = 48,
       this.onLoading,
       this.placeholderBackgroundColor,
       this.placeholderTextColor})
-      : super(key: key);
+      : assetName =
+            'packages/circle_flags/assets/jovial/${countryCode.toLowerCase()}.si',
+        super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,49 +31,8 @@ class CircleFlag extends StatelessWidget {
       height: size,
       child: ScalableImageWidget.fromSISource(
         si: ScalableImageSource.fromSI(
-            DefaultAssetBundle.of(context),
-            'packages/circle_flags/assets/jovial/${countryCode.toLowerCase()}.si'
-        ),
-        onLoading: onLoading ?? _buildPlaceholder,
-        onError: _buildPlaceholder,
-      ),
-    );
-  }
-
-  Widget _buildPlaceholder(BuildContext context) {
-    return _Placeholder(
-      size: size,
-      countryCode: countryCode,
-      backgroundColor: placeholderBackgroundColor,
-      textColor: placeholderTextColor,
-    );
-  }
-}
-
-class _Placeholder extends StatelessWidget {
-  final double size;
-  final String countryCode;
-  final Color? backgroundColor;
-  final Color? textColor;
-
-  const _Placeholder({Key? key, required this.size, required this.countryCode, this.backgroundColor, this.textColor}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Container(
-      width: size,
-      height: size,
-      padding: const EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: backgroundColor ?? Theme.of(context).colorScheme.primaryContainer,
-      ),
-      child: Text(
-        countryCode,
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          color: textColor ?? Theme.of(context).colorScheme.onPrimaryContainer,
+          DefaultAssetBundle.of(context),
+          assetName,
         ),
       ),
     );
